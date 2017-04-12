8699241c8618809b361ef7c209ab96e26f898163
farhan687 <farhan687@gmail.com>
2017-04-12 16:36:16 +0530

    Update stories

--- stories/reactivesearch/NestedList.stories.js
+++ stories/reactivesearch/NestedList.stories.js
@@ -4,33 +4,14 @@ import { ReactiveBase, NestedList, ReactiveList, AppbaseSensorHelper as helper }
 export default class NestedListDefault extends Component {
 	constructor(props) {
 		super(props);
-		this.onData = this.onData.bind(this);
 	}
 
 	componentDidMount() {
 		helper.ResponsiveStory();
 	}
 
-	onData(res) {
-		let result = null;
-		if (res) {
-			let combineData = res.currentData;
-			if (res.mode === "historic") {
-				combineData = res.currentData.concat(res.newData);
-			} else if (res.mode === "streaming") {
-				combineData = helper.combineStreamData(res.currentData, res.newData);
-			}
-			if (combineData) {
-				result = combineData.map((markerData) => {
-					const marker = markerData._source;
-					return this.itemMarkup(marker, markerData);
-				});
-			}
-		}
-		return result;
-	}
-
-	itemMarkup(marker, markerData) {
+	onData(markerData) {
+		const marker = markerData._source;
 		return (
 			<a
 				className="full_row single-record single_record_for_clone"
