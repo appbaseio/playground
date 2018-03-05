9e9f45d54483f0e232e3c828902b4ba15de33802
Divyanshu Maithani <div.blackcat@gmail.com>
2018-03-06 00:03:13 +0530

    Add DatePicker dark story

--- stories/index.js
+++ stories/index.js
@@ -111,6 +111,7 @@ import ReactiveComponentStory from "./reactivesearch/ReactiveComponent.stories";
 import DarkStory from "./reactivesearch/Dark.stories";
 import DarkCard from "./reactivesearch/DarkCard.stories";
 import TagCloudDark from "./reactivesearch/TagCloudDark.stories";
+import DatePickerDark from "./reactivesearch/DatePickerDark.stories";
 
 require("./styles.css");
 
@@ -4079,4 +4080,7 @@ storiesOf("theme", module)
 	))
 	.add("Dark Preset with TagCloud/ToggleButton/Single/MultiDataList", () => (
 		<TagCloudDark themePreset={select('themePreset', ['light', 'dark'], 'dark')} />
+	))
+	.add("Dark Preset with DatePicker", () => (
+		<DatePickerDark themePreset={select('themePreset', ['light', 'dark'], 'dark')} />
 	))
---
+++ stories/reactivesearch/DatePickerDark.stories.js
@@ -0,0 +1,83 @@
+import React, { Component } from "react";
+import moment from "moment";
+import {
+	ReactiveBase,
+	DatePicker,
+	ResultCard,
+	SelectedFilters
+} from "@appbaseio/reactivesearch";
+import ResponsiveStory from "./ResponsiveStory";
+
+export default class DatePickerDefault extends Component {
+	componentDidMount() {
+		ResponsiveStory();
+	}
+
+	dateQuery(value, props) {
+		let query = null;
+		if (value) {
+			query = [
+				{
+					"range": {
+						[props.dataField]: {
+							"lte": moment(value).format("YYYYMMDD")
+						}
+					}
+				}
+			];
+		}
+		return query;
+	}
+
+	onData(res) {
+		return {
+			image: res.image,
+			title: res.name,
+			description: (
+				<div>
+					<div>${res.price}</div>
+					<span style={{ "backgroundImage": `url(${res.host_image})` }}></span>
+					<p>{res.room_type} · {res.accommodates} guests</p>
+				</div>
+			),
+			url: res.listing_url
+		};
+	}
+
+	render() {
+		return (
+			<ReactiveBase
+				app="housing"
+				credentials="0aL1X5Vts:1ee67be1-9195-4f4b-bd4f-a91cd1b5e4b5"
+				type="listing"
+				{...this.props}
+			>
+				<div className={`row ${this.props.themePreset}`}>
+					<div className="col">
+						<DatePicker
+							componentId="DateSensor"
+							dataField="date_from"
+							customQuery={this.dateQuery}
+							initialMonth={this.props.defaultSelected ? null : new Date("2017-05-05")}
+						/>
+					</div>
+
+					<div className="col">
+						<SelectedFilters componentId="DateSensor" />
+						<ResultCard
+							componentId="SearchResult"
+							dataField="name"
+							from={0}
+							size={40}
+							onData={this.onData}
+							showPagination={true}
+							react={{
+								and: ["DateSensor"]
+							}}
+						/>
+					</div>
+				</div>
+			</ReactiveBase>
+		);
+	}
+}
