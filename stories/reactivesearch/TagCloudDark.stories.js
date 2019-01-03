e2cd2f5399a51174f9b0dccc8a53e238e640660d
Lakhan Samani <lakhan.m.samani@gmail.com>
2019-01-03 16:14:50 +0530

    Add count stories for Single/MultiDataList (#42)
    - Added showCount story
    - Added renderListItem story
    - Updated stories using old meetup_app

--- stories/reactivesearch/TagCloudDark.stories.js
+++ stories/reactivesearch/TagCloudDark.stories.js
@@ -7,9 +7,8 @@ export default class TagCloudDefault extends Component {
 	render() {
 		return (
 			<ReactiveBase
-				app="meetup_demo"
-				credentials="LPpISlEBe:2a8935f5-0f63-4084-bc3e-2b2b4d1a8e02"
-				type="meetupdata1"
+				app="meetup_app"
+				credentials="lW70IgSjr:87c5ae16-73fb-4559-a29e-0a02760d2181"
 				{...this.props}
 			>
 				<div className={`row ${this.props.themePreset}`}>
