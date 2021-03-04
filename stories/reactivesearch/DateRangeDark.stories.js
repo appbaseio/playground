db1e1973fe9356061a178439fa860839198e0c6c
bietkul <kuldepsaxena155@gmail.com>
2021-03-05 04:24:28 +0530

    fix: arc URL

--- stories/reactivesearch/DateRangeDark.stories.js
+++ stories/reactivesearch/DateRangeDark.stories.js
@@ -41,7 +41,7 @@ export default class DateRangeDefault extends Component {
 		return (
 			<ReactiveBase
 				app="airbeds-test-app"
-				url="https://a03a1cb71321:75b6603d-9456-4a5a-af6b-a487b309eb61@appbase-demo-ansible-abxiydt-arc.searchbase.io.searchbase.io"
+				url="https://a03a1cb71321:75b6603d-9456-4a5a-af6b-a487b309eb61@appbase-demo-ansible-abxiydt-arc.searchbase.io"
 				enableAppbase
 				type="listing"
 				{...this.props}
