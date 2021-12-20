02ee809274f3d7b84126f135436ed2375da50b09
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2021-12-20 16:57:54 +0530

    Update stories/index.js
    Co-authored-by: Siddharth Kothari <siddharth@appbase.io>

--- stories/index.js
+++ stories/index.js
@@ -4836,7 +4836,7 @@ storiesOf("Range components/RangeSlider", module)
     () => (
       <RangeSliderRSDefault
         dataField="timestamp"
-        title="Date Ranges"
+        title="Date Range Example with queryFormat prop"
         range={{
           start: new Date('2020-05-05'),
           end: new Date('2021-05-05'),
