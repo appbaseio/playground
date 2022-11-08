c17254e05da559a0c77cb57cdc61718e581e313a
Shubham Shah <shubhamshahrising@gmail.com>
2022-11-08 16:04:34 +0530

    feat: use updated index

--- stories/reactivesearch/DateRange.stories.js
+++ stories/reactivesearch/DateRange.stories.js
@@ -40,7 +40,7 @@ export default class DateRangeDefault extends Component {
 	render() {
 		return (
 			<ReactiveBase
-				app="airbeds-test-app"
+				app="clone-airbeds"
 				url="https://a03a1cb71321:75b6603d-9456-4a5a-af6b-a487b309eb61@appbase-demo-ansible-abxiydt-arc.searchbase.io"
 				enableAppbase
 			>
