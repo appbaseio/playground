90d657cc747432d4c2c54355f4dc638828a1173c
Shubham Shah <shubhamshahrising@gmail.com>
2023-08-22 11:00:48 +0530

    chore: remove unnecessary log

--- stories/reactivesearch/SearchBoxWithDocumentSuggestions.stories.js
+++ stories/reactivesearch/SearchBoxWithDocumentSuggestions.stories.js
@@ -10,7 +10,7 @@ import {
 
 import './movie-store.css';
 
-const Main = (props) => console.log(props)||(
+const Main = (props) => (
 	<ReactiveBase
 		app="movies-store-app"
 		url="https://a03a1cb71321:75b6603d-9456-4a5a-af6b-a487b309eb61@appbase-demo-ansible-abxiydt-arc.searchbase.io"
